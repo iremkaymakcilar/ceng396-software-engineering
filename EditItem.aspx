3a0f6e1137fb24ef05b16e638d27f11b4bc1f675
debruparlak <48918029+debruparlak@users.noreply.github.com>
2019-05-31 10:18:56 +0300

    Add files via upload

---
+++ EditItem.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/After.Master" AutoEventWireup="true" CodeBehind="EditItem.aspx.cs" Inherits="minwin.EditItem" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <section style="margin-left:200px">
+        <table>
+           <tr>
+               <b>Title:</b><asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+               <br />
+               <b>Description: </b><asp:Label ID="Label2" runat="server" Text="Label"></asp:Label>
+               <br />
+               <b>Exchange: </b> <asp:CheckBox ID="CheckBox1" runat="server" />
+               <asp:Image ID="Image1" runat="server" />
+           </tr>
+        </table>
+        <br />
+        <form action="/" method="post" runat="server">
+        <asp:Button ID="Button1" class="btn btn-primary custom" runat="server" OnClick="Button1_Click" Text="Delete" />
+            <br />
+            <br />
+            <br />
+        <asp:Button ID="Button2" class="btn btn-primary custom" runat="server" OnClick="Button2_Click" Text="Edit" />
+        </form>
+    </section>
+</asp:Content>
---
+++ ProfilUp.aspx.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace minwin
+{
+    public partial class ProfilUp : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var cur_user = (user)Session["current_user"];
+            Session["current_user"] = cur_user;
+            using (var myDb = new ceng396())
+            {
+                var user = (from u in myDb.user
+                            where u.user_id == cur_user.user_id
+                            select u).Single();
+                LabelFname.Text = user.fname;
+                LabelLname.Text = user.lname;
+                LabelEmail.Text = user.email;
+                LabelUserName.Text = user.username;
+                LabelDep.Text = user.major;
+                LabelFac.Text = user.faculty;
+                LabelSchool.Text = user.school;
+                TextBoxPass.Text = cur_user.password;
+                TextBoxUserName.Text = cur_user.username;
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {           
+                HttpPostedFile postedfile = FileUpload1.PostedFile;
+                string filename = Path.GetFileName(postedfile.FileName);
+                string fileextension = Path.GetExtension(filename);
+
+                if (fileextension.ToLower() == ".jpg" || fileextension.ToLower() == ".jpeg" || fileextension.ToLower() == ".png")
+                {
+                    Stream stream = postedfile.InputStream;
+                    BinaryReader binaryreader = new BinaryReader(stream);
+                    byte[] bytes = binaryreader.ReadBytes((int)stream.Length);
+
+                    var cur_user = (user)Session["current_user"];
+                    Session["current_user"] = cur_user;
+                    using (var myDb = new ceng396())
+                    {
+                        var user = (from u in myDb.user
+                                    where u.user_id == cur_user.user_id
+                                    select u).Single();
+                    if (TextBoxUserName.Text != null) { 
+                        user.username = TextBoxUserName.Text;
+                    }
+                    if (TextBoxPass.Text != null) { 
+                    user.password = TextBoxPass.Text;
+                    }
+                    if (postedfile != null)
+                        {
+                            user.profile_photo = bytes;
+                        }
+                        
+                        myDb.SaveChanges();
+                    }
+
+                
+                Response.Redirect("Profil.aspx");
+            }
+        }
+    }
+}
