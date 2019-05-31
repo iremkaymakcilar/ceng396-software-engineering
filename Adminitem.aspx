2ee506cd005624d9053d5548e71f4e4d30d6bdf7
iremkaymakcilar <38735984+iremkaymakcilar@users.noreply.github.com>
2019-05-31 19:02:35 +0300

    Add files via upload

---
+++ Adminitem.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Admin.Master" AutoEventWireup="true" CodeBehind="Adminitem.aspx.cs" Inherits="minwin.Adminitem" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+     <section style="margin-left:200px">
+        <asp:Image ID="Image1" runat="server" /><br />
+        <br />
+        <br />
+               <b><h3>Title:</h3></b><asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+               <br />
+               <b><h3>Description: </h3></b><asp:Label ID="Label2" runat="server" Text="Label"></asp:Label>
+               <br />
+               
+                   
+               
+                           
+        <br />
+        <form action="/" method="post" runat="server">
+            <b>Exchange: </b> <asp:CheckBox ID="CheckBox1" runat="server" />
+            <br /><br /><br />
+        <asp:Button ID="Button1" class="btn btn-primary" Width="200px" runat="server" OnClick="Button1_Click" Text="Send Message To Owner" />
+            <br />
+            <br />
+        </form>
+    </section>
+</asp:Content>
---
+++ Adminitem.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace minwin
+{
+    public partial class Adminitem : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var current_user = (user)Session["current_user"];
+            Session["current_user"] = current_user;
+            if (current_user != null)
+            {
+                Session["current_user"] = current_user;
+                using (var myDb = new ceng396())
+                {
+                    int itemid = Convert.ToInt32(Request.QueryString["id"]);
+                    var itm = (from u in myDb.item
+                               where u.item_id == itemid
+                               select u).Single();
+                    var currentimg = (from u in myDb.image
+                                      where u.item_id == itm.item_id
+                                      select u).Single();
+
+                    int id = Convert.ToInt32(Request.QueryString["id"]);
+                    Label1.Text = itm.title;
+                    Label2.Text = itm.description;
+                    Image1.ImageUrl = "data:image;base64," + Convert.ToBase64String(currentimg.img);
+                    Image1.Height = 300;
+                    Image1.Width = 300;
+                }
+            }
+            else
+            {
+                Response.Redirect("Login.aspx");
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            var current_user = (user)Session["current_user"];
+            Session["current_user"] = current_user;
+            Response.Redirect("Chat.aspx");
+        }
+    }
+}
