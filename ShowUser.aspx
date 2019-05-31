c32247099bb1920cb36bc4782ddb805be4410ce3
Hümeyra <48918019+humeyraunal@users.noreply.github.com>
2019-05-31 10:19:10 +0300

    Add files via upload

---
+++ ShowUser.aspx
@@ -0,0 +1,3 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Admin.Master" AutoEventWireup="true" CodeBehind="ShowUser.aspx.cs" Inherits="minwin.ShowUser" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+</asp:Content>
---
+++ ShowUser.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace minwin
+{
+    public partial class ShowUser : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            using (var myDb = new ceng396())
+            {
+                var allusers = (from u in myDb.user
+                                select u).ToList();
+
+                ContentPlaceHolder c = Page.Master.FindControl("ContentPlaceHolder1") as ContentPlaceHolder;
+
+                foreach (var x in allusers)
+                {
+                    Label label = new Label();
+                    label.Text = x.fname + " " + x.lname + " (" + x.username + ") " + " <br>";
+                    c.Controls.Add(label);
+                }
+            }
+        }
+    }
+}
